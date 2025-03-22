7f9fa9c78c3bbc678f7dc40ec5a591497fc2640a
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-23 03:20:44 +0800

    no message

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,11 @@
+#include "Application.h"
+#include <stdio.h>
+
+namespace Hazel {
+	Application::Application() {}
+	Application::~Application() {}
+	void Application::Run() {
+		printf("Run");
+        while (true);
+	}
+}
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Hazel/Application.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+extern Hazel::Application* Hazel::CreateApplication();
+int main(int argc, char** argv)
+{
+    printf("Hello World!\n");
+    auto app = Hazel::CreateApplication();
+    app->Run();
+    delete app;
+}
+
+#endif // 
