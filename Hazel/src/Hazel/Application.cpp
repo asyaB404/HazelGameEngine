5b6963d3ea687b9eb262a5f9e2895e44b00e4293
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-23 22:09:18 +0800

    BaseEvent

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,11 +1,23 @@
 #include "Application.h"
-#include <stdio.h>
+#include "Log.h"
+#include "Events/ApplicationEvent.h"
+
 
 namespace Hazel {
 	Application::Application() {}
 	Application::~Application() {}
 	void Application::Run() {
-		printf("Run");
+		HZ_CORE_INFO("Application Started");
+        WindowResizeEvent e(1280, 720);
+        if (e.IsInCategory(EventCategoryApplication))
+        {
+            HZ_TRACE(e);
+        }
+        if (e.IsInCategory(EventCategoryInput))
+        {
+            HZ_TRACE(e);
+        }
+
         while (true);
 	}
 }
