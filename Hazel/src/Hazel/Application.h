92e23ecb91e51af1978dba22fa789bad30755344
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-25 17:46:06 +0800

    Dispatching events from GLFW.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
+#include "Hazel/Events/ApplicationEvent.h"
 
 namespace Hazel {
 
@@ -11,6 +13,12 @@ namespace Hazel {
 		Application();
 		virtual ~Application();
 		void Run();
+		void OnEvent(Event& e);
+	
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
     Application* CreateApplication();
